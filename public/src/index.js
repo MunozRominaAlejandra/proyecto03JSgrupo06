7315e6857c43798a8b36ba4f726946af70e37869
SebastianBarbito <sabastianbarbito0@gmail.com>
2025-09-13 19:31:26 -0300

    contador visible, se agrego un div en el html, se agrego unas lineas de codigo en el index.js y el contador es funcinal señalando el numero total de mascotas, si estan vacunadas o no

---
+++ public/src/index.js
@@ -0,0 +1,31 @@
+import { contarMascotas } from "./componentes/conteo.js" ;
+import {mascotas, anotarMascota} from "./componentes/mascotas.js";
+const contadorDiv = document.getElementById("contador");
+const formulario = document.getElementById("formulario");
+
+function mostrarContador() {
+  const conteo = contarMascotas(mascotas);
+  contadorDiv.innerHTML = `
+    <p>Total de mascotas: <b>${conteo.total}</b></p>
+    <p>Vacunadas: <b>${conteo.vacunadas}</b></p>
+    <p>No vacunadas: <b>${conteo.noVacunadas}</b></p>
+  `;
+}
+mostrarContador();
+
+formulario.addEventListener("submit", function(e) {
+
+    e.preventDefault();
+    const nombre = document.getElementById("nombre").value;
+    const tipo = document.getElementById("tipo").value;
+    const edad = document.getElementById("edad").value;
+    const dueño = document.getElementById("dueño").value;
+    const vacuna = document.querySelector("input[name='Vacuna']:checked").value;
+
+    const mascota=anotarMascota(nombre, tipo, edad, dueño, vacuna);
+    mascotas.push(mascota);
+
+    console.log(mascotas);
+    mostrarContador();
+    formulario.reset();
+});
